990874abf2a3922fb7f52d4cf0b2a012ef0e3ed3
Sajid <asajid451@gmail.com>
2024-08-12 09:42:56 +0500

    Added form io with basic component

---
+++ src/components/FormComponents/email/EmailField.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import InputComponent from '../sharedComponents/Input';
+
+export default class EmailField extends  InputComponent {}
---
+++ src/components/FormComponents/panel/Panel.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FormioComponentsList } from "../../";
+import { View } from "react-native";
+import { Card } from "@rneui/themed";
+import PropTypes from "prop-types";
+import { StyleSheet } from "react-native";
+import styles from "./styles";
+
+const Panel = (props) => {
+  const title =
+    props.component.title && !props.component.hideLabel
+      ? props.component.title
+      : undefined;
+  const titleStyle = {
+    ...StyleSheet.flatten(styles.title),
+    color: props.colors.secondaryTextColor,
+  };
+
+  return (
+    <Card containerStyle={styles.panel}>
+      <Card.Title style={titleStyle}>{title}</Card.Title>
+      <View style={styles.componentsWrapper}>
+        <FormioComponentsList
+          {...props}
+          components={props.component.components}
+        ></FormioComponentsList>
+      </View>
+    </Card>
+  );
+};
+
+Panel.propTypes = {
+  component: PropTypes.object,
+  theme: PropTypes.object,
+  colors: PropTypes.object,
+};
+
+export default Panel;
